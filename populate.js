017f6e412c49f0a1b68cad5d4a7e0e9ddc1c5c01
Marcos Vinicius do Carmo Sousa <marcos.vinicius.wz@gmail.com>
2017-12-22 14:44:34 -0200

    populate script better

--- populate.js
+++ populate.js
@@ -7,7 +7,7 @@ var async = require('async');
 var total = parseInt(process.argv[2]) || 50;
 var reset = false || !!process.argv[3];
 var successEntries = 0;
-
+var connection_type = process.env.MONGODB_PATH_ONLINE;
 /**
  Initilize enviorments configuration
  */
@@ -22,8 +22,20 @@ mongoose.Promise = global.Promise;
 /**
  Connect to mongodb ONLINE BY DEFAULT check .env files
  */
-var db = mongoose.connect(process.env.MONGODB_PATH_ONLINE,{ useMongoClient: true }, function() {
-	if ((parseInt(process.argv[2]) > 0 && process.argv[3] == 'r') || !(!!process.argv[2])){
+function readArgs () {
+	if ((process.argv[2]) == 'L' || (process.argv[3]) == 'L' || (process.argv[4]) == 'L'|| (process.argv[5]) == 'L') {
+		connection_type = process.env.MONGODB_PATH_LOCAL;
+	}
+	else {
+		connection_type = process.env.MONGODB_PATH_ONLINE;
+	}
+}
+
+readArgs();
+var db = mongoose.connect(connection_type,{ useMongoClient: true }, function() {
+	str = 'You are connected to mongodb at:\n' + connection_type;
+	console.log(str);
+	if ((parseInt(process.argv[2]) > 0 && process.argv[3] == 'r') || !(!!process.argv[2]) || process.argv[2] == 'L'){
 		console.log('You can check avaiable commands running: node populate.js h');
 		console.log('Reseting db');
 		console.log('Mongodb collection dropped (reset)');
@@ -32,19 +44,23 @@ var db = mongoose.connect(process.env.MONGODB_PATH_ONLINE,{ useMongoClient: true
 		});
 	}
 	else if (process.argv[2] == 'r' || parseInt(process.argv[2]) == 0) {
+		console.log('You can check avaiable commands running: node populate.js h');
+		console.log('Reseting db');
 		console.log('Mongodb collection dropped (reset)');
 		mongoose.connection.db.dropDatabase (function () {
 			finishConnection();
 		});
 	}
-	else if ((parseInt(process.argv[2]) && !(!!process.argv[3]))) {
+	else if ((parseInt(process.argv[2]) && !(!!process.argv[3])) || parseInt(process.argv[2]) && process.argv[3] == 'L') {
+		console.log('You can check avaiable commands running: node populate.js h');
 		pop();
 	}
 	else if (process.argv[2] == 'h') {
 		str = 'Avaliable populate commands:\n' +
-		 '\'node populate.js <X>    -- to include X new entries in db \'\n' +
+		 '\'node populate.js <X>   -- to include X new entries in db \'\n' +
 		 '\'node populate.js <X> r  -- to reset and populate with X new entries in db \'\n' +
-		 '\'node populate.js r      -- to reset db\'';
+		 '\'node populate.js r      -- to reset db\'\n' +
+		 '\'TO RUN LOCALY: Put L argument after your arguments (requires mongo db set up and running localy) \'\n';
 		console.log(str);
 		finishConnection();
 	}
@@ -52,7 +68,8 @@ var db = mongoose.connect(process.env.MONGODB_PATH_ONLINE,{ useMongoClient: true
 		str = 'Wrong arguments, please run\n' +
 		 '\'node populate.js <X>    -- to include X new entries in db \'\n' +
 		 '\'node populate.js <X> r  -- to reset and populate with X new entries in db \'\n' +
-		 '\'node populate.js r      -- to reset db\'';
+		 '\'node populate.js r      -- to reset db\'\n' +
+		 '\'TO RUN LOCALY: Put L argument after your arguments (requires mongo db set up and running localy) \'\n';
 		console.log(str);
 		finishConnection();
 	}
